e36120d39d9bcfdd397d475344af31b12570f44f
anhdh-3069 <duong.hoang.anh@sun-asterisk.com>
2022-05-11 15:51:11 +0700

    chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.user.email_regex
 
+  attr_accessor :remember_token
+
   before_save :email_downcase
 
   validates :email, presence: true,
@@ -13,18 +15,40 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def email_downcase
-    email.downcase!
-  end
-
   class << self
-    def self.digest string
+    def digest string
       cost = if ActiveModel::SecurePassword.min_cost
                BCrypt::Engine::MIN_COST
              else
                BCrypt::Engine.cost
              end
-      BCrypt::Password.create(string, cost: cost)
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  private
+
+  def email_downcase
+    email.downcase!
+  end
+
 end
